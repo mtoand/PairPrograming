a8d9331851dce103de1b22df02312aa606852fe3
sasami0005 <g16905gs@gm.tsuda.ac.jp>
2019-05-17 17:24:33 +0900

    テストフォルダ作りました。

---
+++ test/pairPrograming/IntToEngTest.java
@@ -0,0 +1,14 @@
+package pairPrograming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntToEngTest {
+
+	@Test
+	public void testで数値を英訳する() {
+		fail("Not yet implemented");
+	}
+
+}
